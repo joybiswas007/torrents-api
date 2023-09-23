5747594ed0e0e040cbea6409478acb5b49f2357f
Joy Biswas <joybiswas040701@gmail.com>
2023-09-23 21:32:08 +0600

    added knaben

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Torrents API
-A collection of bunch of public trackers api;
+A collection of public torrents trackers API written in JavaScript;
 
 ## WIP
-Added Knaben but need to be fix other than that all routes work fine;
+
 Lots of more public trackers api will be added soon; Feel free to send pull requests;
 
 ## Requirements
+
 Navigate to the cloned directory
 
-Install dependencies: ``` npm install ```
+Install dependencies: `npm install`
 
 Create a `.env` file inside the directory and fill in all the details.
 
@@ -19,13 +20,35 @@ PORT=PORTNUMBER //IF NO port is set then default port is 10000;
 _1337X=https://1337x.to
 BITSEARCH=https://bitsearch.to
 TOR_LOCK=https://www.torlock.com
+KNABEN=https://knaben.eu
 
 ```
 
 ## Usage
-To run the api locally, use the following command: ``` npm run dev ```
+
+To run the api locally, use the following command: `npm run dev`
+And request method must be POST; and request body should be like this:
+
+```
+{
+    "search": "search query"
+}
+
+http://localhost:10000/torrents/api/v1/knaben
+Knaben route takes two properties
+
+{
+    "search": "search query",
+    "mode: "fast" or "live"
+}
+
+Between Fast and Live mode try it out :p
+and when searching on knaben on few search results you'll see `https://knaben.eu/live/dl/rutracker/?` this types of links well fret NOT just add them to your client and it'll automatically starts downloading like other magnet urls;
+
+```
 
 ## Available routes
+
 ```
 http://localhost:10000/torrents/api/v1/1337x
 http://localhost:10000/torrents/api/v1/bitsearch
--- routes/knaben.js
+++ routes/knaben.js
@@ -3,28 +3,32 @@ const router = express.Router();
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-router.post("/", async(req, res) => {
-    const { search } = req.body;
-    try {
-        const search_url = `${process.env.KNABEN}${search}&search=fast`;
-        const response = await axios.get(search_url);
-        const $ = cheerio.load(response.data);
-        const $element = $(".table");
-        let torrents = [];
-        for(let i = 0; i < $element.length; i++){
-            console.log($element.length);
-            const torrent = $element[i];
-            const total = $(torrent).find("p").text().trim();
-            const magnet = $(torrent).find(".text-wrap a").attr("href"); 
-            torrents.push({
-                total,
-                magnet,
-            });
-        }
-        res.send(torrents);
-    } catch (error) {
-        res.status(500).send({error: "undefined"})
+router.post("/", async (req, res) => {
+  const { search, mode } = req.body;
+  try {
+    const search_url = `${process.env.KNABEN}/search/index.php?cat=000000000&q=${search}&search=${mode}`;
+    const response = await axios.get(search_url);
+    const $ = cheerio.load(response.data);
+    const $element = $(".table tbody");
+    let torrents = [];
+    for (const torrent of Array.from($element.find("tr"))) {
+      const torrent_name = $(torrent).find(".text-wrap a").text().trim();
+      const magnet = $(torrent).find(".text-wrap a").attr("href");
+      const size = $(torrent).find(".text-nowrap td").eq(2).text();
+      const seeders = $(torrent).find(".text-nowrap td").eq(4).text();
+      const leechers = $(torrent).find(".text-nowrap td").eq(5).text();
+      torrents.push({
+        torrent_name,
+        magnet,
+        size,
+        seeders,
+        leechers,
+      });
     }
+    res.send(torrents);
+  } catch (error) {
+    res.status(500).send({ error: "undefined" });
+  }
 });
 
 module.exports = router;
