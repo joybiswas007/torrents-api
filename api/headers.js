2ac2f5056f4fc0ba678775a9c5da36d98a4b9905
Joy Biswas <joybiswas040701@gmail.com>
2023-10-15 09:19:11 +0600

    fixed small issue

--- api/headers.js
+++ api/headers.js
@@ -1,5 +1,6 @@
 const headers = {
   headers: {
+    "Content-Type": "text/html",
     "User-Agent":
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
   },
